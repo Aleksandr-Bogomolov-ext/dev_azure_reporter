0f693e09b222680ae5cbb4b0f511b164503230db
Aleksandr-Bogomolov <120007043+Aleksandr-Bogomolov@users.noreply.github.com>
2022-12-29 20:10:12 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="dev_azure_reporter",
+    version="0.1",
+    description="A simple command-line tool to write fields of WorkItems at dev.azure.com",
+    author="Aleksandr Bogomolov",
+    author_email="aleksandr_bogomolov@epam.com",
+    url="https://github.com/Aleksandr-Bogomolov-ext/dev_azure_reporter"
+)
