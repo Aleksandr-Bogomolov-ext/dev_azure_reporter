0d1c364b417e0ff7c8adefbff0630bb74a198925
Aleksandr-Bogomolov <120007043+Aleksandr-Bogomolov@users.noreply.github.com>
2023-01-02 21:04:53 +0600

    heavy refactoring

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name="dev_azure_reporter",
-    version="0.1",
+    version="0.2",
     description="A simple command-line tool to write fields of WorkItems at dev.azure.com",
     author="Aleksandr Bogomolov",
-    author_email="aleksandr_bogomolov@epam.com",
-    url="https://github.com/Aleksandr-Bogomolov-ext/dev_azure_reporter"
+    author_email="aleksandr-bogomolov-ext@bayer.com",
+    url="https://github.com/bayer-int/dats-dev_azure_reporter/"
 )
